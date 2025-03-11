0de8ff4d89ed3e0dc7a9e6c4ef726ea8a8c8439f
ambertan <ambertjn@gmail.com>
2025-03-11 23:35:38 +0800

    Modify the parameters of the delete command
    Delete command takes in an index of a list as a parameter.
    
    Using the name and phone number of a contact to be deleted is more
    intuitive and user-friendly.
    
    Let's
    * update the parameters of the delete command to take in a name
    and phone number, instead of an index
    * update the usage message of the delete command to reflect the changes

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 
 import java.util.List;
 
@@ -9,7 +11,9 @@ import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
 
 /**
  * Deletes a person identified using it's displayed index from the address book.
@@ -19,16 +23,22 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Deletes the person identified by the index number used in the displayed person list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + ": Deletes the person identified by their full name and phone number.\n"
+            + "Parameters: "
+            + PREFIX_NAME + "NAME "
+            + PREFIX_PHONE + "PHONE "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_NAME + "John Doe "
+            + PREFIX_PHONE + "98765432 ";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
-    private final Index targetIndex;
+    private final Name targetName;
+    private final Phone targetPhone;
 
-    public DeleteCommand(Index targetIndex) {
-        this.targetIndex = targetIndex;
+    public DeleteCommand(Name targetName, Phone targetPhone) {
+        this.targetName = targetName;
+        this.targetPhone = targetPhone;
     }
 
     @Override
@@ -36,11 +46,21 @@ public class DeleteCommand extends Command {
         requireNonNull(model);
         List<Person> lastShownList = model.getFilteredPersonList();
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        Person personToDelete = null;
+        for (int i = 0; i < lastShownList.size(); i++) {
+            Person target = lastShownList.get(i);
+            boolean matchesName = target.getName().equals(this.targetName);
+            boolean matchesPhone = target.getPhone().equals(this.targetPhone);
+            if (matchesName && matchesPhone) {
+                personToDelete = target;
+                break;
+            }
         }
 
-        Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
+        if (personToDelete == null) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_NAME_AND_PHONE);
+        }
+
         model.deletePerson(personToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
     }
@@ -57,13 +77,16 @@ public class DeleteCommand extends Command {
         }
 
         DeleteCommand otherDeleteCommand = (DeleteCommand) other;
-        return targetIndex.equals(otherDeleteCommand.targetIndex);
+        boolean matchesName = targetName.equals(otherDeleteCommand.targetName);
+        boolean matchesPhone = targetPhone.equals(otherDeleteCommand.targetPhone);
+        return (matchesName && matchesPhone);
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this)
-                .add("targetIndex", targetIndex)
+                .add("targetName", targetName)
+                .add("targetPhone", targetPhone)
                 .toString();
     }
 }
