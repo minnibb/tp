4eb8e29b1c1622983526570f80cf27a0f7d73aab
minnibb <97654250+minnibb@users.noreply.github.com>
2025-03-13 01:07:31 +0800

    Update ModelManagerTest.java

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -142,7 +142,8 @@ public class ModelManagerTest {
         // Get initial order
         String firstPersonNameBefore = testModel.getFilteredPersonList().get(0).getName().toString();
         // Sort by name in reverse order
-        testModel.updateSortedPersonList((p1, p2) -> p2.getName().toString().compareToIgnoreCase(p1.getName().toString()));
+        testModel.updateSortedPersonList((
+            p1, p2) -> p2.getName().toString().compareToIgnoreCase(p1.getName().toString()));
         // Get new order
         String firstPersonNameAfter = testModel.getFilteredPersonList().get(0).getName().toString();
         // Verify sort worked (names should be different if sort changed order)
