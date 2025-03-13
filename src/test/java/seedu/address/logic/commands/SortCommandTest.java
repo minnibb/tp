ebcb8c53b3b081a05d4e3ccf7202407808fb1bc0
minnibb <97654250+minnibb@users.noreply.github.com>
2025-03-13 16:33:52 +0800

    Update SortCommandTest.java
    Fixing errors for Java CI checks

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Path;
 import java.util.Comparator;
-import java.util.Arrays;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
