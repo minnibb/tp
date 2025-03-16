55879ebe31f55269aba3595b9ac2a55b0481ec9a
ambertan <ambertjn@gmail.com>
2025-03-17 01:43:25 +0800

    GradeTest.java: Add more test cases

--- src/test/java/seedu/address/model/person/GradeTest.java
+++ src/test/java/seedu/address/model/person/GradeTest.java
@@ -20,22 +20,52 @@ public class GradeTest {
     }
 
     @Test
-    public void isValidName() {
+    public void isValidGrade() {
         // null name
         assertThrows(NullPointerException.class, () -> Grade.isValidGrade(null));
 
         // invalid name
         assertFalse(Grade.isValidGrade("")); // empty string
         assertFalse(Grade.isValidGrade(" ")); // spaces only
-        assertFalse(Grade.isValidGrade("Pri 7")); // not within Pri 1-6 and Sec 1-5
+        assertFalse(Grade.isValidGrade("Pri 7")); // not within Pri 1-6
+        assertFalse(Grade.isValidGrade("Sec 0")); // not within Sec 1-5
 
-        // valid name
+        // valid grade - primary
         assertTrue(Grade.isValidGrade("pri 1")); // no capital letters
         assertTrue(Grade.isValidGrade("Pri 1")); // mix of capital and small letters
         assertTrue(Grade.isValidGrade("PRI 1")); // all capital letters
+        assertTrue(Grade.isValidGrade("pri 2")); // no capital letters
+        assertTrue(Grade.isValidGrade("Pri 2")); // mix of capital and small letters
+        assertTrue(Grade.isValidGrade("PRI 2")); // all capital letters
+        assertTrue(Grade.isValidGrade("pri 3")); // no capital letters
+        assertTrue(Grade.isValidGrade("Pri 3")); // mix of capital and small letters
+        assertTrue(Grade.isValidGrade("PRI 3")); // all capital letters
+        assertTrue(Grade.isValidGrade("pri 4")); // no capital letters
+        assertTrue(Grade.isValidGrade("Pri 4")); // mix of capital and small letters
+        assertTrue(Grade.isValidGrade("PRI 4")); // all capital letters
+        assertTrue(Grade.isValidGrade("pri 5")); // no capital letters
+        assertTrue(Grade.isValidGrade("Pri 5")); // mix of capital and small letters
+        assertTrue(Grade.isValidGrade("PRI 5")); // all capital letters
+        assertTrue(Grade.isValidGrade("pri 6")); // no capital letters
+        assertTrue(Grade.isValidGrade("Pri 6")); // mix of capital and small letters
+        assertTrue(Grade.isValidGrade("PRI 6")); // all capital letters
+
+        // valid grade - secondary
         assertTrue(Grade.isValidGrade("sec 1")); // no capital letters
         assertTrue(Grade.isValidGrade("Sec 1")); // mix of capital and small letters
         assertTrue(Grade.isValidGrade("SEC 1")); // all capital letters
+        assertTrue(Grade.isValidGrade("sec 2")); // no capital letters
+        assertTrue(Grade.isValidGrade("Sec 2")); // mix of capital and small letters
+        assertTrue(Grade.isValidGrade("SEC 2")); // all capital letters
+        assertTrue(Grade.isValidGrade("sec 3")); // no capital letters
+        assertTrue(Grade.isValidGrade("Sec 3")); // mix of capital and small letters
+        assertTrue(Grade.isValidGrade("SEC 3")); // all capital letters
+        assertTrue(Grade.isValidGrade("sec 4")); // no capital letters
+        assertTrue(Grade.isValidGrade("Sec 4")); // mix of capital and small letters
+        assertTrue(Grade.isValidGrade("SEC 4")); // all capital letters
+        assertTrue(Grade.isValidGrade("sec 5")); // no capital letters
+        assertTrue(Grade.isValidGrade("Sec 5")); // mix of capital and small letters
+        assertTrue(Grade.isValidGrade("SEC 5")); // all capital letters
     }
 
     @Test
