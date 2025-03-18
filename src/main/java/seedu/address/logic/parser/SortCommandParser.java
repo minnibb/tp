d0ffed2f1552bddec9fe6ab360e19a5bbd55187c
minnibb <97654250+minnibb@users.noreply.github.com>
2025-03-18 20:54:43 +0800

    Update SortCommandParser.java
    Fix import statement in SortCommandParser

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -1,6 +1,6 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.commands.SortCommand;
