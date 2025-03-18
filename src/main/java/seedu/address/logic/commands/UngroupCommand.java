0528c28f3b184ec3c2c6ba258658ea17c0a8d6c9
liy722 <e1300504@u.nus.edu>
2025-03-18 13:02:28 +0800

    Add Ungroup feature

--- src/main/java/seedu/address/logic/commands/UngroupCommand.java
+++ src/main/java/seedu/address/logic/commands/UngroupCommand.java
@@ -1,2 +1,31 @@
-package seedu.address.logic.commands;public class Ungroup {
+// UngroupCommand.java
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.Model;
+
+/**
+ * Resets grouping/filtering to show all persons.
+ */
+public class UngroupCommand extends Command {
+    public static final String COMMAND_WORD = "ungroup";
+    public static final String MESSAGE_SUCCESS = "Showing all contacts (no grouping).";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(Model.PREDICATE_SHOW_ALL_PERSONS);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof UngroupCommand;
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
--- src/test/java/seedu/address/logic/commands/UngroupCommandTest.java
+++ src/test/java/seedu/address/logic/commands/UngroupCommandTest.java
@@ -1,2 +1,38 @@
-package seedu.address.logic.commands;public class UngroupCommandTest {
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.Role;
+
+public class UngroupCommandTest {
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private final Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute_resetFilter_showsAllPersons() {
+        model.updateFilteredPersonList(person -> person.getRole().equals(new Role("Student")));
+
+        assertCommandSuccess(
+                new UngroupCommand(),
+                model,
+                UngroupCommand.MESSAGE_SUCCESS,
+                expectedModel
+        );
+    }
+
+    @Test
+    public void execute_emptyFilter_remainsUnchanged() {
+        assertCommandSuccess(
+                new UngroupCommand(),
+                model,
+                UngroupCommand.MESSAGE_SUCCESS,
+                expectedModel
+        );
+    }
 }
