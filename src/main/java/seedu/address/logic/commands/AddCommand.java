de06feaba491ba9c1cc64d287a0f0bae11ec2537
ambertan <ambertjn@gmail.com>
2025-03-19 23:24:11 +0800

    Fix typo in usage message for Add Command

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -42,7 +42,7 @@ public class AddCommand extends Command {
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_TAG + "friends "
-            + PREFIX_TAG + "owesMoney"
+            + PREFIX_TAG + "owesMoney "
             + PREFIX_ROLE + "Parent";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
