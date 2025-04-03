9c45a93993383375b92b432232b5f55b871e1380
Brian Goh <naeunonly1999@gmail.com>
2025-04-03 21:58:33 +0800

    edit list command message when list is empty

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -11,14 +11,18 @@ import seedu.address.model.Model;
 public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
-
     public static final String MESSAGE_SUCCESS = "Listed all persons";
-
+    public static final String MESSAGE_NO_CONTACTS = "No contacts to show";
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        if (model.getFilteredPersonList().isEmpty()) {
+            return new CommandResult(MESSAGE_NO_CONTACTS);
+        }
+
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
