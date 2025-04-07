7cc5e9b97caf65529d285dcc23f4e124d9932071
ambertan <ambertjn@gmail.com>
2025-04-07 18:00:23 +0800

    AddCommand.java: Edit message for duplicate persons

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -53,7 +53,7 @@ public class AddCommand extends Command {
             + PREFIX_CLASS + "1B";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
-    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in ClassHive";
 
     private final Person toAdd;
 
